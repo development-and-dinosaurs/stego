a0a46c2d386035182c01f1fb970d6ff625a8221c
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2025-10-26 07:17:57 +0000

    Add tests for action mapper

--- serialisation/core/kotlinx/src/commonTest/kotlin/uk/co/developmentanddinosaurs/stego/serialisation/kotlinx/ActionMapperTest.kt
+++ serialisation/core/kotlinx/src/commonTest/kotlin/uk/co/developmentanddinosaurs/stego/serialisation/kotlinx/ActionMapperTest.kt
@@ -4,65 +4,87 @@ import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
-import uk.co.developmentanddinosaurs.stego.statemachine.Context
-import uk.co.developmentanddinosaurs.stego.statemachine.Event
-import uk.co.developmentanddinosaurs.stego.statemachine.LogAction
-import uk.co.developmentanddinosaurs.stego.statemachine.StateMachineException
+import uk.co.developmentanddinosaurs.stego.statemachine.*
+import kotlin.reflect.KClass
+
+private data class CustomActionDto(val data: String) : ActionDto
+
+private data class CustomAction(val data: String) : Action {
+    override fun execute(
+        context: Context,
+        event: Event,
+    ): Context = context
+}
 
 class ActionMapperTest : BehaviorSpec({
+    Given("an ActionMapper with an empty registry") {
+        val mapper = ActionMapper(emptyMap())
 
-    var loggedMessage: String?
-    val testLogger: (String) -> Unit = { message -> loggedMessage = message }
+        and("a built-in AssignActionDto") {
+            val dto = AssignActionDto("key", StringDataValueDto("value"))
 
-    Given("a LogActionMapper") {
-        val mapper = LogActionMapper(testLogger)
+            When("the dto is mapped") {
+                val action = mapper.map(dto)
 
-        When("mapping a LogActionDto") {
-            val dto = LogActionDto("Hello from test!")
-            val action = mapper.map(dto)
-
-            Then("it should return a LogAction") {
-                action.shouldBeInstanceOf<LogAction>()
+                Then("it should return an AssignAction") {
+                    action.shouldBeInstanceOf<AssignAction>()
+                }
             }
+        }
 
-            Then("the LogAction should use the logger when executed") {
-                loggedMessage = null // Reset logger for this test
-                action.execute(Context(), Event("TEST_EVENT"))
-                loggedMessage shouldBe "LogAction: Hello from test!"
+        and("a built-in LogActionDto") {
+            val dto = LogActionDto("message")
+
+            When("the dto is mapped") {
+                val action = mapper.map(dto)
+
+                Then("it should return a LogAction") {
+                    action.shouldBeInstanceOf<LogAction>()
+                }
+            }
+        }
+
+        and("an unknown ActionDto") {
+            val dto = CustomActionDto("some data")
+
+            When("the dto is mapped") {
+                Then("it should throw a StateMachineException") {
+                    val exception = shouldThrow<StateMachineException> { mapper.map(dto) }
+                    exception.message shouldBe "Action DTO type 'CustomActionDto' not found in registry and is not a built-in type."
+                }
             }
         }
     }
 
-    Given("a CompositeActionMapper with a LogActionMapper") {
-        val compositeMapper = CompositeActionMapper(
-            mapOf(
-                LogActionDto::class to LogActionMapper(testLogger),
-                AssignActionDto::class to AssignActionMapper()
-            )
-        )
+    Given("an ActionMapper with a custom action in the registry") {
+        val registry = mapOf<KClass<out ActionDto>, (ActionDto) -> Action>(CustomActionDto::class to { dto: ActionDto -> CustomAction((dto as CustomActionDto).data) })
+        val mapper = ActionMapper(registry)
+        val dto = CustomActionDto("custom data")
 
-        When("mapping a LogActionDto") {
-            val dto = LogActionDto("Composite Map Test")
-            val action = compositeMapper.map(dto)
+        When("the custom dto is mapped") {
+            val action = mapper.map(dto)
 
-            Then("it should return a LogAction") {
-                action.shouldBeInstanceOf<LogAction>()
-            }
-
-            Then("the LogAction should use the logger when executed") {
-                loggedMessage = null // Reset logger for this test
-                action.execute(Context(), Event("COMPOSITE_EVENT"))
-                loggedMessage shouldBe "LogAction: Composite Map Test"
+            Then("it should return the correct custom action from the registry") {
+                action.shouldBeInstanceOf<CustomAction>()
+                action.data shouldBe "custom data"
             }
         }
+    }
 
-        When("mapping an unknown ActionDto type") {
-            val dummyDto = object : ActionDto {}
+    Given("an ActionMapper with a registry that shadows a built-in type") {
+        val customAssignAction = CustomAction("shadowed")
+        val registry =
+            mapOf<KClass<out ActionDto>, (ActionDto) -> Action>(
+                AssignActionDto::class to { customAssignAction },
+            )
+        val mapper = ActionMapper(registry)
+        val dto = AssignActionDto("key", StringDataValueDto("value"))
 
-            Then("it should throw a StateMachineException") {
-                shouldThrow<StateMachineException> {
-                    compositeMapper.map(dummyDto)
-                }
+        When("the built-in dto is mapped") {
+            val action = mapper.map(dto)
+
+            Then("it should return the built-in action, not the one from the registry") {
+                action.shouldBeInstanceOf<AssignAction>()
             }
         }
     }

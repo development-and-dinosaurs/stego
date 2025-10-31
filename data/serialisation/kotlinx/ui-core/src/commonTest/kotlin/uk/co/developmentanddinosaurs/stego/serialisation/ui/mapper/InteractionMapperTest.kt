d92e61a79ec06d4912bf3ea48a6de93468b2aa84
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2025-10-31 06:06:59 +0000

    Add test for interaction mapper

---
+++ data/serialisation/kotlinx/ui-core/src/commonTest/kotlin/uk/co/developmentanddinosaurs/stego/serialisation/ui/mapper/InteractionMapperTest.kt
@@ -0,0 +1,23 @@
+package uk.co.developmentanddinosaurs.stego.serialisation.ui.mapper
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import uk.co.developmentanddinosaurs.stego.serialisation.ui.node.InteractionDto
+
+class InteractionMapperTest : BehaviorSpec({
+    Given("an InteractionMapper") {
+        val mapper = InteractionMapper()
+
+        and("an InteractionDto") {
+            val dto = InteractionDto(trigger = "some-event")
+
+            When("the dto is mapped") {
+                val interaction = mapper.map(dto)
+
+                Then("it should map the trigger correctly") {
+                    interaction.trigger shouldBe "some-event"
+                }
+            }
+        }
+    }
+})

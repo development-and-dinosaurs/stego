d12e9a0bb40b80cb7655077242c1eb759647cc56
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2025-11-01 07:04:12 +0000

    Disable annoying ktlint rules
    This is mainly to stop the supertype going to the next line,
    which isn't cool in the test classes.
    
    More tweaking in the future, maybe.

--- buildSrc/src/main/kotlin/spotless-convention.gradle.kts
+++ buildSrc/src/main/kotlin/spotless-convention.gradle.kts
@@ -7,6 +7,8 @@ spotless {
         target("src/**/*.kt")
         ktlint().editorConfigOverride(
             mapOf(
+                "ktlint_standard_class-signature" to "disabled",
+                "ktlint_standard_multiline-expression-wrapping" to "disabled",
                 "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
             ),
         )

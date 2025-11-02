0921027655bf5513992c30443864bde3e64dd68a
Tyrannoseanus <tyrannoseanus@developmentanddinosaurs.co.uk>
2025-11-02 13:31:59 +0000

    Replace column with annotation

--- domain/ui-core/src/commonMain/kotlin/uk/co/developmentanddinosaurs/stego/ui/node/ColumnUiNode.kt
+++ domain/ui-core/src/commonMain/kotlin/uk/co/developmentanddinosaurs/stego/ui/node/ColumnUiNode.kt
@@ -2,6 +2,7 @@ package uk.co.developmentanddinosaurs.stego.ui.node
 
 import uk.co.developmentanddinosaurs.stego.annotations.StegoNode
 
+@StegoNode(type = "stego.column")
 data class ColumnUiNode(
     override val id: String,
     val children: List<UiNode>,
